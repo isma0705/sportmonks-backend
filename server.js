e42f3615d385f93a301c44b6d0e000339b950454
isma0705 <tradewihtexness@gmail.com>
2025-11-03 15:56:47 +0100

    Fix TOKEN assignment in server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-const TOKEN = ' process.env.SPORTMONKS_TOKEN';
+const TOKEN = const TOKEN = process.env.SPORTMONKS_TOKEN;
 
 app.get('/livescores', async (req, res) => {
   try {
